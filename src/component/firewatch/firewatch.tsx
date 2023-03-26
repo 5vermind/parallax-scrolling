dba2490510db961c76a61c2b79e24688f3234770
5vermind <5vermind@gmail.com>
2023-03-26 22:14:31 +0900

    svg

--- src/component/firewatch/firewatch.tsx
+++ src/component/firewatch/firewatch.tsx
@@ -1,12 +1,8 @@
-import BackgroundImage from '@/background-image.svg'
+import BackgroundImage from 'public/background-image.svg'
 
 export const Firewatch = () => {
   return (
     <div className="firewatch">
-      <div className="firewatch__container">
-        <div className="firewatch__div1">1</div>
-        <div className="firewatch__div2">2</div>
-      </div>
       <BackgroundImage />
     </div>
   )
